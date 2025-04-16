45686b1ff22840fb2e2febcb61d49bae84b24293
Nick <58723340+NickNcikNick@users.noreply.github.com>
2025-04-15 22:50:20 -0500

    .

--- main/wifi_ap.c
+++ main/wifi_ap.c
@@ -17,7 +17,7 @@
    If you'd rather not, just change the below entries to strings with
    the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
 */
-#define EXAMPLE_ESP_WIFI_SSID      "The Artbot"
+#define EXAMPLE_ESP_WIFI_SSID      "The Nickbot"
 #define EXAMPLE_ESP_WIFI_PASS      "isfordrawing"
 #define EXAMPLE_ESP_WIFI_CHANNEL   CONFIG_ESP_WIFI_CHANNEL
 #define EXAMPLE_MAX_STA_CONN       CONFIG_ESP_MAX_STA_CONN
@@ -42,7 +42,8 @@ void wifi_init_softap(void)
 {
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_ap();
+    esp_netif_t *netif = esp_netif_create_default_wifi_ap();
+    esp_netif_dhcps_start(netif);  // Starts DHCP server if not already
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
